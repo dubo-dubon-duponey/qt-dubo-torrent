0c6a015bf0d001c79815023c15b394fae77f91e5
dubo-dubon-duponey <dubodubonduponey+github@pm.me>
2012-04-10 17:38:15 +0200

    Fix timestamp typage to avoid boost dep
    Signed-off-by: dubo-dubon-duponey <dubodubonduponey+github@pm.me>

--- qtltalert.h
+++ qtltalert.h
@@ -14,7 +14,7 @@ class QTLIBTORRENTSHARED_EXPORT QtltAlert : public QObject
 {
     Q_OBJECT
 public:
-    explicit QtltAlert(const QString &what, const QString &message, const int category, const u_int64_t timestamp, QObject *parent = 0);
+    explicit QtltAlert(const QString &what, const QString &message, const int category, const qint64 timestamp, QObject *parent = 0);
 
     Q_PROPERTY(QString      what        READ what)
     Q_PROPERTY(QString      message     READ message)
@@ -30,7 +30,7 @@ private:
     QString         _message;
     QString         _what;
     int             _category;
-    u_int64_t       _timestamp;
+    qint64       _timestamp;
 
 };
 
